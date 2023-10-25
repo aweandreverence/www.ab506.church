827b05cabee84cd6e7a96ad2153151c893331da8
Peniel Mesele <pinimes@gmail.com>
2023-10-25 09:59:47 +0300

    Changed the home page text

--- pages/index.js
+++ pages/index.js
@@ -5,7 +5,7 @@ import Page from '../components/base_page';
 export default function Home() {
     return (
         <Page>
-            <h1>www.ab506.Church</h1>
+            <h1>AB506</h1>
         </Page>
     );
 }
